48d023917247c6e98431c5f95223710cc9353fec
Sumit <sumitpal.spal@gmail.com>
2021-08-07 12:28:46 +0200

    Added product component

---
+++ vue/src/components/Product.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="product shadow">
+    <img :src="img" :alt="name" class="product__img"/>
+    <div class="p-3">
+      <div class="d-flex flex-row justify-content-between flex-wrap flex-md-nowrap">
+        <p class="product__name">{{name}}</p>
+        <p class="fw-bold text-primary">{{$filters.formatPrice(price)}}</p>
+      </div>
+
+      <p class="d-none d-md-block">{{$filters.limitCharacters(description,50)}}...</p>
+      <div class="d-flex flex-row justify-content-between align-items-start flex-wrap">
+        <p class="product__stock d-none d-md-block">{{stock}} left</p>
+
+        <button class="btn btn-sm btn-primary text-uppercase" :disabled="stock <= 0"
+                @click="$emit('addToCart(id)')">+ Add to cart</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Product',
+  props: {
+    id: {
+      required: true,
+      type: String,
+    },
+    name: {
+      required: true,
+      type: String,
+    },
+    img: {
+      required: true,
+      type: String,
+    },
+    description: {
+      required: true,
+      type: String,
+    },
+    price: {
+      required: true,
+      type: Number,
+    },
+    stock: {
+      required: true,
+      type: Number,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import "~bootstrap/scss/functions";
+@import "~bootstrap/scss/variables";
+
+.product {
+  &__name {
+    font-weight: bold;
+    min-height: 2em;
+  }
+
+  &__img {
+    width: 100%;
+  }
+
+  &__stock {
+    background: grey;
+    padding: 4px 14px 4px 15px;
+    color: white;
+    border-radius: $border-radius;
+  }
+}
+</style>
