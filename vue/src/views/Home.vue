032da3e3192cf916fc03b35551c89529d410c0ef
Sumit <sumitpal.spal@gmail.com>
2021-08-07 12:39:06 +0200

    Modified home component to show product list

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-12 col-sm-4 col-md-4 col-lg-3 mb-5"
+           v-for="product in products" v-bind:key="product.id">
+        <Product :id="product.id" :description="product.productDescription" :img="product.image_url"
+                 :name="product.productName" :price="product.price"
+                 :stock="product.stock"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Product from '@/components/Product.vue';
+import ActionNames from '@/store/ActionNames';
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
+  components: { Product },
+  computed: {
+    products() {
+      return this.$store.state.products;
+    },
+  },
+  created() {
+    this.$store.dispatch(ActionNames.GetProducts);
   },
 });
 </script>
