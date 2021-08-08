6a7c978fb59f5a0511397271003bab19aa730aa2
Sumit <sumitpal.spal@gmail.com>
2021-08-08 20:31:27 +0200

    Deleted About page and Added cart page for mobile

---
+++ vue/src/views/Cart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="cart container">
+    <cart-products></cart-products>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CartProducts from '@/components/CartProducts.vue';
+import ActionNames from '@/store/ActionNames';
+
+export default defineComponent({
+  name: 'Cart',
+  components: { CartProducts },
+  created() {
+    this.$store.dispatch(ActionNames.GetCartProducts);
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.cart {
+  padding-top: 15px;
+}
+</style>
